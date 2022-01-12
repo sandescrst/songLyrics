e005db693630f04b98e2c52cfcd7b1354e3df7e6
sandescrst <sandescrst@gmail.com>
2022-01-12 16:04:11 -0600

    backend completed

---
+++ homepage/services.py
@@ -0,0 +1,9 @@
+# import requests
+# import json
+
+# def get_lyrics():
+#     artist_name = str(request.GET.get('artist_name'))
+#     song = str(request.GET.get('song'))
+#     apiurl= 'https://api.lyrics.ovh/v1/' + artist_name + '/' + song
+#     req = requests.get(apiurl)
+#     json_data= req.json()
---
+++ homepage/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+import json
+
+# Create your views here.
+def home(request):
+    
+    
+    return render(request, 'homepage/home.html')
+
+def lyrics(request):
+    artist_name = str(request.GET.get('artist_name'))
+    song = str(request.GET.get('song'))
+    apiurl= 'https://api.lyrics.ovh/v1/' + artist_name + '/' + song
+    req = requests.get(apiurl)
+    json_data= req.json()['lyrics']
+    ly=json_data
+    return render(request, 'homepage/lyrics.html',{'lyric': ly})
